feb90bfdb58fe109bca37e52c61098d4c6d95abb
ChristerNilsson <janchrister.nilsson@gmail.com>
2016-10-05 22:52:12 +0200

    x

---
+++ ZZ/A72_Assert/L2.py
@@ -0,0 +1,50 @@
+# y = kx+m
+
+###################
+
+def L2():
+
+    def d(x): return 00
+    assert d(0) == 1 
+    assert d(1) == 1 
+    assert d(2) == 1 
+
+    def e(x): return 00
+    assert e(0) == 100 
+    assert e(1) == 100 
+    assert e(2) == 100 
+
+    def f(x): return 00
+    assert f(0) == 0 
+    assert f(1) == 1 
+    assert f(2) == 2 
+    
+    def g(x): return 00
+    assert g(0) == 1
+    assert g(1) == 2
+    
+    def h(x): return 00
+    assert h(0) == 0
+    assert h(1) == 2
+    assert h(2) == 4
+    
+    def i(x): return 00
+    assert i(0) == 1
+    assert i(1) == 3
+    assert i(2) == 5
+    
+    def j(x): return 00
+    assert j(0) == 0
+    assert j(1) == 10
+    assert j(2) == 20
+    
+    def k(x): return 00
+    assert k(0) == 10
+    assert k(1) == 30
+    assert k(2) == 50
+    
+    def l(x): return 00
+    assert l(0) == 11
+    assert l(1) == 22
+    assert l(2) == 33
+    
---
+++ ZZ_Grupp Mon 1530/A72_Assert/A72_Assert.pyde
@@ -0,0 +1,8 @@
+from L1 import L1
+from L2 import L2
+from L3 import L3
+from L4 import L4
+
+L1()
+
+print "Klart!"
